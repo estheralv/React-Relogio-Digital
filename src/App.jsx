28e6d6331ca5376c34e51da1420e6c5aa30e7657
Esther <125416224+estheralv@users.noreply.github.com>
2024-12-10 19:36:01 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import RelogioDigital from './assets/components/RelogioDigital'
+
+function App() {
+  return (
+    <>
+    <RelogioDigital/>
+    </>
+  )
+}
+
+export default App
